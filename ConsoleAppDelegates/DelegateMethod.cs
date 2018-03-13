8bbb72e035f4df0ed514d9af736eb4a664491c9a
Gerardo Sullivan <g_j_sullivan@hotmail.com>
2018-03-14 12:05:32 +1300

    Initial commit

---
+++ ConsoleAppDelegates/DelegateMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDelegates
+{
+    class DelegateMethod
+    {
+        public static void DelMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
